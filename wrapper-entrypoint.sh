eadad0226bbd594e759a2194c782f1d89c23d78e
Mohammad Hossein Ebadollahi <mhemrg120@gmail.com>
2019-03-08 18:15:02 +0330

    extract and add php.ini applier to Dockerfile

--- wrapper-entrypoint.sh
+++ wrapper-entrypoint.sh
@@ -2,14 +2,6 @@
 
 set -m
 
-if [ -e /var/www/html/liara_php.ini ]; \
-  then \
-    echo 'Applying custom liara_php.ini...'; \
-    mv /var/www/html/liara_php.ini /usr/local/etc/php/conf.d/liara_php.ini; \
-  else \
-    echo 'Applying default php.ini...'; \
-fi
-
 /usr/local/bin/docker-entrypoint.sh apache2-foreground &
 
 chown -R www-data:www-data /var/www/html/
